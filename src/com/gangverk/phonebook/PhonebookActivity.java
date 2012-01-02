0b72e2f6dc6c2bc7506aa23f20960667f7f3eb63
Hordur Hauksson <hordur.hauks@gmail.com>
2012-01-02 11:42:00 +0000

    Initial commit

---
+++ src/com/gangverk/phonebook/PhonebookActivity.java
@@ -0,0 +1,17 @@
+package com.gangverk.phonebook;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class PhonebookActivity extends Activity {
+    private final static String LOG_TAG = "PhonebookActivity"; 
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        int a = 2;
+        Log.d(LOG_TAG,String.format("the number was %d", a));
+    }
+}
