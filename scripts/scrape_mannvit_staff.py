b8994fce01ac6fab367c577b7bfa09ba0e98e9b8
Hörður Hauksson <hordur.hauks@gmail.com>
2012-01-12 13:03:16 +0000

    Managing git

--- scripts/scrape_mannvit_staff.py
+++ scripts/scrape_mannvit_staff.py
@@ -106,6 +106,7 @@ def main():
 	c.execute("""CREATE TABLE "android_metadata" ("locale" TEXT DEFAULT 'en_US')""")
 	c.execute("""CREATE TABLE division(_id INTEGER PRIMARY KEY, name VARCHAR(256))""")
 	c.execute("""CREATE TABLE employee(_id INTEGER PRIMARY KEY, name VARCHAR(256), title VARCHAR(256), phone VARCHAR(64), mobile VARCHAR(64), email VARCHAR(256), workplace_id INTEGER, division_id INTEGER,FOREIGN KEY (workplace_id) REFERENCES workplace(_id), FOREIGN KEY (division_id) REFERENCES division(_id))""")
+	c.execute("""CREATE INDEX nIndex ON employee(name)""")
 	c.execute("""CREATE TABLE workplace(_id INTEGER PRIMARY KEY, address VARCHAR(256))""")
 	c.execute("""CREATE VIEW employeeInfo AS SELECT e._id AS _id, e.name AS employee, e.title AS title, e.phone AS phone, e.mobile AS mobile, e.email AS email, w.address AS workplace, d.name AS division FROM employee e, workplace w, division d WHERE w._id = e.workplace_id AND d._id = e.division_id""")
 	for workplace_name, workplace_id in workplaces.iteritems():
