a755c4e265d269a1d4f8732288c1ec3bb1b6143e
Gardar Hauksson <gardarh@gangverk.is>
2012-01-26 09:53:58 +0000

    Made script more production friendly

--- scripts/scrape_mannvit_staff.py
+++ scripts/scrape_mannvit_staff.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python
 BASE_URL = "http://mannvit.is/"
-BASE_PATH = "htmlfiles/"
 STAFF_URL = "%sMannvit/Starfsmenn/" % (BASE_URL,)
-DBFILE = "mannvit_staff.sqlite"
+FILENAME_TEMPLATE= 'mannvit'
 
 from lxml import etree
-import urllib2, codecs, os.path, sqlite3, re
+import urllib2, codecs, os, os.path, sqlite3, re, sys, tempfile, hashlib, json
 
 REX_FOREIGN = re.compile("(\(?\+\d+\)?)")
 REX_NON_NUMERIC= re.compile("([^\d]+)")
 
-def page_to_tree(url,filename):
+def page_to_tree(url,debug=False,basepath=None):
+	'''basepath and filename only required when debugging'''
 	parser = etree.HTMLParser(recover=True)
-	if not os.path.isdir(BASE_PATH):
-		os.makedirs(BASE_PATH)
-	full_filename = '%s%s' % (BASE_PATH,filename)
-	#print "FF",full_filename
-	if not os.path.isfile(full_filename):
+	md5_filename = '%s-%s.html' % (FILENAME_TEMPLATE, hashlib.md5(url).hexdigest())
+	filepath = '%s' % (tempfile.gettempdir(),)
+	if debug and basepath:
+		filepath = basepath
+		if not os.path.isdir(basepath):
+			os.makedirs(basepath)
+	full_filename = '%s/%s' % (filepath,md5_filename)
+
+	if not debug or (debug and not os.path.isfile(full_filename)):
 		raw_html = urllib2.urlopen(url).read()
-		print "Downloading %s..." % (url,)
 		f = open(full_filename,'w')
 		f.write(raw_html)
 		f.close()
 	f = codecs.open(full_filename, encoding='UTF-8')
 	raw_html = f.read()
 	f.close()
+	if not debug:
+		os.remove(full_filename)
 
 	return etree.fromstring(raw_html, parser)
 
@@ -45,23 +50,93 @@ def parse_phone(number):
 	localnum = REX_NON_NUMERIC.sub('',localnum)
 	return '+%s %s' % (countrypart,localnum) if len(localnum) > 0 else ''
 
+def print_usage_exit():
+	sys.exit('Usage: ./%s --output=OUTPUT_PATH [--debug --htmlcachepath=DEBUG_BASEPATH]' % (os.path.basename(sys.argv[0]),))
+
 def main():
-	main_filename = 'mannvit_staff.html'
-	tree = page_to_tree(STAFF_URL, main_filename)
+	basepath = None
+	debug = False
+	output_path = None
+	for arg in sys.argv[1:]:
+		value = None
+		argsplit = arg.split('=',1)
+		parname = argsplit[0]
+		if len(argsplit) > 1:
+			value = argsplit[1]
+
+		if parname == '--debug':
+			debug = True
+		elif parname == '--output':
+			output_path = value
+		elif parname == '--htmlcachepath':
+			basepath = value
+		else:
+			print 'Unknown parameter specified: %s' % (parname,)
+			print_usage_exit()
+
+	if not output_path or (debug and not basepath):
+		print 'Missing output_path or debug set without basepath'
+		print_usage_exit()
+
+	employee_data = get_employees(debug,basepath)
+
+	json_filename = '%s/%s.json' % (output_path, FILENAME_TEMPLATE)
+	old_json = None
+	try:
+		old_json = json.loads(open(json_filename,'r').read())
+	except (ValueError,IOError):
+		pass
+
+	if json.loads(json.dumps(employee_data)) != json.loads(json.dumps(old_json)):
+		print "DB changed"
+		conn = sqlite3.connect('%s/%s.sqlite' % (output_path,FILENAME_TEMPLATE))
+		c = conn.cursor()
+		c.execute('''DROP TABLE IF EXISTS division''')
+		c.execute('''DROP TABLE IF EXISTS employee''')
+		c.execute('''DROP TABLE IF EXISTS workplace''')
+		c.execute('''DROP TABLE IF EXISTS android_metadata''')
+		c.execute('''DROP VIEW IF EXISTS employeeInfo''')
+
+		c.execute("""CREATE TABLE "android_metadata" ("locale" TEXT DEFAULT 'en_US')""")
+		c.execute("""CREATE TABLE division(_id INTEGER PRIMARY KEY, name VARCHAR(256))""")
+		c.execute("""CREATE TABLE employee(_id INTEGER PRIMARY KEY, name VARCHAR(256), title VARCHAR(256), phone VARCHAR(64), mobile VARCHAR(64), email VARCHAR(256), workplace_id INTEGER, division_id INTEGER,FOREIGN KEY (workplace_id) REFERENCES workplace(_id), FOREIGN KEY (division_id) REFERENCES division(_id))""")
+		c.execute("""CREATE INDEX nIndex ON employee(name)""")
+		c.execute("""CREATE TABLE workplace(_id INTEGER PRIMARY KEY, address VARCHAR(256))""")
+		c.execute("""CREATE VIEW employeeInfo AS SELECT e._id AS _id, e.name AS employee, e.title AS title, e.phone AS phone, e.mobile AS mobile, e.email AS email, w.address AS workplace, d.name AS division FROM employee e LEFT OUTER JOIN workplace w ON w._id = e.workplace_id LEFT OUTER JOIN division d ON d._id = e.division_id""")
+		for workplace_name, workplace_id in employee_data['workplaces'].iteritems():
+			c.execute("""INSERT INTO workplace (_id,address) VALUES (?,?)""", (workplace_id, workplace_name))
+
+		for employee in employee_data['employees']:
+			c.execute("""INSERT INTO employee(_id,name,title,phone,mobile,email,workplace_id,division_id) VALUES (?,?,?,?,?,?,?,?)""", (employee['id'],employee['name'],employee.get('title',''),employee.get('phone',''),employee.get('mobile',''),employee.get('email',''),employee.get('workplace',0),0))
+		c.execute('''INSERT INTO "android_metadata" VALUES('is_IS')''')
+		conn.commit()
+		c.execute('VACUUM')
+		c.close()
+		conn.close()
+
+		f = open(json_filename,'w')
+		f.write(json.dumps(employee_data))
+		f.close()
+	else:
+		print "No DB change"
+
+def get_employees(debug,basepath):
+	tree = page_to_tree(STAFF_URL, debug, basepath)
 	workplaces = {}
 	divisions = {}
 
 	staff_div = tree.find('.//div[@class="staff_search"]/div[@class="list"]')
 	employees = []
-	for row in staff_div.findall('.//tr'):
+	all_staff = staff_div.findall('.//tr')
+	for staff_i,row in enumerate(all_staff):
 		titleover = False
 		cols = row.findall('./td')
 		if len(cols) == 3:
+			print "Fetching employee info (%d/%d)" % (staff_i+1,len(all_staff))
 			stafflink = '%s%s' % (BASE_URL,cols[0].find('./a').get('href'))
 			staff_id = int(stafflink.split('/')[-1])
-			staff_filename = 'staff_%d.html' % (staff_id,)
 
-			employee_tree = page_to_tree(stafflink, staff_filename)
+			employee_tree = page_to_tree(stafflink, debug, basepath)
 			employee_info = employee_tree.find('.//div[@class="starfsmenn_info"]')
 			employee_data = employee_info.findall('./*')
 			employee = {}
@@ -125,30 +200,8 @@ def main():
 			employees.append(employee)
 		else:
 			print "NOT 3 COLS!"
-	conn = sqlite3.connect(DBFILE)
-	c = conn.cursor()
-	c.execute('''DROP TABLE IF EXISTS division''')
-	c.execute('''DROP TABLE IF EXISTS employee''')
-	c.execute('''DROP TABLE IF EXISTS workplace''')
-	c.execute('''DROP TABLE IF EXISTS android_metadata''')
-	c.execute('''DROP VIEW IF EXISTS employeeInfo''')
 
-	c.execute("""CREATE TABLE "android_metadata" ("locale" TEXT DEFAULT 'en_US')""")
-	c.execute("""CREATE TABLE division(_id INTEGER PRIMARY KEY, name VARCHAR(256))""")
-	c.execute("""CREATE TABLE employee(_id INTEGER PRIMARY KEY, name VARCHAR(256), title VARCHAR(256), phone VARCHAR(64), mobile VARCHAR(64), email VARCHAR(256), workplace_id INTEGER, division_id INTEGER,FOREIGN KEY (workplace_id) REFERENCES workplace(_id), FOREIGN KEY (division_id) REFERENCES division(_id))""")
-	c.execute("""CREATE INDEX nIndex ON employee(name)""")
-	c.execute("""CREATE TABLE workplace(_id INTEGER PRIMARY KEY, address VARCHAR(256))""")
-	c.execute("""CREATE VIEW employeeInfo AS SELECT e._id AS _id, e.name AS employee, e.title AS title, e.phone AS phone, e.mobile AS mobile, e.email AS email, w.address AS workplace, d.name AS division FROM employee e LEFT OUTER JOIN workplace w ON w._id = e.workplace_id LEFT OUTER JOIN division d ON d._id = e.division_id""")
-	for workplace_name, workplace_id in workplaces.iteritems():
-		c.execute("""INSERT INTO workplace (_id,address) VALUES (?,?)""", (workplace_id, workplace_name))
-
-	for employee in employees:
-		c.execute("""INSERT INTO employee(_id,name,title,phone,mobile,email,workplace_id,division_id) VALUES (?,?,?,?,?,?,?,?)""", (employee['id'],employee['name'],employee.get('title',''),employee.get('phone',''),employee.get('mobile',''),employee.get('email',''),employee.get('workplace',0),0))
-	c.execute('''INSERT INTO "android_metadata" VALUES('is_IS')''')
-	conn.commit()
-	c.execute('VACUUM')
-	c.close()
-	conn.close()
-
+	return {'employees':employees,'workplaces':workplaces,'divisions':divisions}
+	
 if __name__ == "__main__":
 	main()
